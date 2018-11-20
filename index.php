b1e7ac3b7eea624abf0dc57f04cbc1d0f1db1bf1
= <=>
2018-11-20 20:20:02 +0530

    1st page of google search engine

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Doodle</title>
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="wrapper indexpage">
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="images/festisite_google.png" alt="googleImage">
+            </div>
+            <form method="get" action="search.php">
+                <div class="searchContianer">
+                    <input class="searchBox" type="text" name="term" id="term">
+                </div>
+                <input class="searchButton" type="submit" value="Search" name="submit">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
