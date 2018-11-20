de69638e2568842b81e7350cd9386c50bf2581f6
= <=>
2018-11-20 20:17:13 +0530

    parser class

---
+++ classes/DomDocumentParser.php
@@ -0,0 +1,29 @@
+<?php 
+class DomDocumentParser {
+    private $doc;
+    public function __construct($url) {
+        $options = array(
+            'http' => array('method'=>"GET",'header'=>"User-Agaent: doobleBot/0.1\n")
+        );
+
+        $context = stream_context_create($options);
+        $this->doc = new DomDocument();
+        @$this->doc->loadHTML(file_get_contents($url,false,$context));
+    }
+    
+    public function getLinks() {
+        return $this->doc->getElementsByTagName("a");
+    }
+    
+    public function getTitle() {
+        return $this->doc->getElementsByTagName("title");
+    }
+
+    public function getMeta() {
+        return $this->doc->getElementsByTagName("meta");
+    }
+
+    public function getImages() {
+        return $this->doc->getElementsByTagName("img");
+    }
+}
